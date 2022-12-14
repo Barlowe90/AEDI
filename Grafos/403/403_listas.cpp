617d0f4f16ac68b6809312aa89d95f6172e3fcc8
Barlowe <67549375+Barlowe90@users.noreply.github.com>
2022-12-14 20:37:06 +0100

    Update 403_listas.cpp
    Working

--- Grafos/403/403_listas.cpp
+++ Grafos/403/403_listas.cpp
@@ -1,39 +1,28 @@
-#include <stdlib.h>  // Funcion exit
-#include <string.h>  // Funcion memset
-#include <iostream>  // Variables cin y cout
-#include <vector>
+#include <stdlib.h>		// Funcion exit
+#include <string.h>  	// Funcion memset
+#include <iostream>  	// Variables cin y cout
+#include <list>
+#include <queue>
+//#include <sstream>
 
 using namespace std;
 
-#define MAX_NODOS 10
+#define MAX_NODOS 20000
 
 //////////////////////////////////////////////////////////////
 ////////////        VARIABLES GLOBALES        ////////////////
 //////////////////////////////////////////////////////////////
 
 int nnodos;                 // Numero de nodos del grafo
-vector<int> G[MAX_NODOS]; 	// Lista de adyacencia
+list<int> G[MAX_NODOS]; 	// Lista de adyacencia
 bool visitado[MAX_NODOS];   // Marcas de nodos visitados
-string num1, num2;
 string linea;
+string num = "";
+queue<int> cola;
 
 //////////////////////////////////////////////////////////////
 ////////////     FUNCIONES DEL PROGRAMA       ////////////////
 //////////////////////////////////////////////////////////////
-
-void funcionSeparadora(string linea, string &num1, string &num2){
-	if (linea.length() == 1){
-		num1 = linea[0];
-		num2 = "0";
-	}else if (linea.length() == 3){
-		num1 = linea[0];
-		num2 = linea[2];
-	}
-}
-
-void addNodo(vector<int> G[], int i, int num){
-	G[i].push_back(num);
-}
 
 void leeGrafo (void){
 	cin >> nnodos;
@@ -42,64 +31,75 @@ void leeGrafo (void){
 		exit(0);
 	}
 	
-	for(int i = 1; i <= MAX_NODOS; i++)
-		memset(G, 0, sizeof(G));
-		
-	cout << "Nodos " << nnodos << endl;
 	cin.ignore();
+
+	for (int i = 0; i < MAX_NODOS; i++)
+		G[i].clear();
 	
-	for (int i = 1; i <= nnodos; i++) {
+	for(int i = 0; i < nnodos; i++){
 		getline(cin, linea);
-		funcionSeparadora(linea, num1, num2);
-		int c1 = stoi(num1);
-		int c2 = stoi(num2);
-		if (c2 == 0){
-			addNodo(G, i, c1);
-		}
-		if (c2 != 0 && c2 < c1){
-			addNodo(G, i, c2);
-			addNodo(G, i, c1);
-		}else if (c2 != 0 && c2 > c1){
-			addNodo(G, i, c1);
-			addNodo(G, i, c2);
+
+		for(int j = 0; j <= (int)linea.length(); j++){
+			if(linea[j] == ' ' || j == (int)linea.length()){
+				G[i].push_back(stoi(num));
+				num = "";
+			}else{
+				num = num + linea[j];
+			}
 		}
 	}
+
 }
 
-void bpp(int v){
+bool bpp(int v){
+	cola.push(v);
 	visitado[v]= true;
-	cout << "Estamos en v: " << v << ". " << endl;
 	
-	for (int w : G[v]){
-		cout << "Recorremos de v: " << v << ". " << endl;
-		cout << "Adyacente w: " << w << endl;
-		if (!visitado[w]){
-			cout << "Adyacente no visitado " << w << endl;
-			bpp(w);
+	list<int>::iterator EA;
+	//EA = G[v - 1].begin();
+	
+	//wile(EA != G[v - 1].end()){
+	for(EA = G[v - 1].begin(); EA != G[v - 1].end(); EA++ ){
+		if(*EA == nnodos){
+			cola.push(*EA);
+			return true;
+		}else if(!visitado[*EA]){
+			if(bpp(*EA))
+				return true;
+			cola.push(v);
 		}
-		cout << "termina for de w " << w << endl;
-    }
-    cout << "Fin bpp v: " << v << endl;
+		//EA++;
+	}
+	return false;
 }
 
 void busquedaPP (void){
 	memset(visitado, 0, sizeof(visitado));		// BorrarMarcas
 
-	for (int v = 1; v <= nnodos; v++){
-		cout << "AH" << endl;
-		if (!visitado[v])
-			bpp(v);
-	}
-	cout << endl;
+	while(!cola.empty())
+		cola.pop();
+
+	if(bpp(1)){
+		cout << cola.size() << endl;
+		while(!cola.empty()){ 
+			cout << cola.front() << endl;
+			cola.pop();
+		}
+	}else
+		cout << "INFINITO" << endl;
 }
 
-void mostrarLista(){
-    for (int i = 1; i <= MAX_NODOS; i++) {
+void mostrarLista(void){
+    list<int>::iterator EA;
+    for(int i = 0; i < nnodos; i++){
         cout << i << " -> ";
-        for (int x : G[i])
-            cout << x << " ";
+        EA = G[i].begin();
+        while(EA != G[i].end()){
+			cout << *EA << " ";            
+			EA++;
+        }
         cout << endl;
-    } 
+    }
 }
 
 //////////////////////////////////////////////////////////////
@@ -109,10 +109,10 @@ void mostrarLista(){
 int main (void){
 	int ncasos;
 	cin >> ncasos;
-	for (int i = 1; i <= ncasos; i++) {
-		cout << "Caso " << i << endl;
+	
+	for (int i = 0; i < ncasos; i++) {
+		cout << "Caso " << i + 1 << endl;
 		leeGrafo();
 		busquedaPP();
-		//mostrarLista();
 	}
 }
